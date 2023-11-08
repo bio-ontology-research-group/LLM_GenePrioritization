7b6137377c326b450a0648c8a666c8133c3f0654
shenay <senaykafkas@hotmail.com>
2023-11-08 12:44:10 +0300

    Add files via upload
    this script is used to create questions for each sample from the clinvar dataset

---
+++ code/questions_clinvar.py
@@ -0,0 +1,44 @@
+from unidecode import unidecode
+
+#cnt	ID      geneSymbol      phenoLabels     phenoClasses    sex     zygosityLabel
+f=open("final_clinvar_dataset.txt","r")
+phens=[]
+genes=[]
+import sys
+import random
+idx=int(sys.argv[1])
+
+#for l in f:
+l=f.readlines()[(idx-1)]
+
+l=unidecode(l)
+p=l.rstrip().split('\t')
+#if len (p)<6:
+#	sys.exit("LEN issue")
+phens.append([p[1],p[2]])
+gene=p[1]
+
+
+#random.shuffle(phens)
+x="A  patient who is suspected of having a genetic disease, presented with these clinical symptoms: "
+for phen in phens[0][1].split('|'):
+	phen=phen[0]+phen[1:]
+	x+='"'+phen+'", '
+x=x[:-2]+'. Rank these genes according to their association with the symptoms of the patient:'
+
+f=open('../hgnc.genes.txt')
+for l in f:
+	p=l.rstrip()
+	if p.isnumeric():
+		continue
+	genes.append(p)
+genes=set(genes)
+genes=list(genes)
+
+for i in range(4, 101, 5):
+	tt=x
+	for gene in set(genes[:i]+[phens[0][0]]):
+		tt+='"'+gene+'", '
+	print(tt[:-2])
+print('correct answer:',phens[0])
+
